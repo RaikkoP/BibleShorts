6deff142bb30f4b942e4097427e03959c8fd7406
RaikkoP <raikko.prants@gmail.com>
2023-12-16 15:01:07 +0200

    Refactored Bible Selection, Now takes user input

--- src/main/java/org/example/BibleAPI.java
+++ src/main/java/org/example/BibleAPI.java
@@ -22,7 +22,7 @@ public class BibleAPI {
         apiRoute = dotenv.get("API_ROUTE");
         apiKey = dotenv.get("API_KEY");
     }
-    public String getBible() throws UnirestException {
+    public String getBible(String userInput) throws UnirestException {
         HttpResponse<JsonNode> response = Unirest.get(this.apiRoute + "/v1/bibles")
                 .header("api-key", this.apiKey)
                 .asJson();
@@ -32,7 +32,7 @@ public class BibleAPI {
             JSONObject obj = dataArray.getJSONObject(i);
             Object parsedObj = obj.get("name");
             parsedObj.toString();
-            if(parsedObj.toString().contains("King James")){
+            if(parsedObj.toString().contains(userInput)){
                 Object getId = obj.get("id");
                 return getId.toString();
             }
@@ -55,10 +55,9 @@ public class BibleAPI {
         System.out.println(result);
         return result;
     }
-    public LinkedHashMap<String, ArrayList<String>> getChapters(ArrayList<String> books, String id) throws UnirestException {
-        LinkedHashMap<String, ArrayList<String>> chapters = new LinkedHashMap<>();
+    public ArrayList<String> getChapters(ArrayList<String> books, String id) throws UnirestException {
+        ArrayList<String> chapters = new ArrayList<>();
         for(String book : books){
-            ArrayList<String> chapList = new ArrayList<>();
             HttpResponse<JsonNode> response = Unirest.get(apiRoute + "/v1/bibles/" + id + "/books/" + book + "/chapters")
                     .header("api-key", this.apiKey)
                     .asJson();
@@ -67,11 +66,13 @@ public class BibleAPI {
             for (int i = 0; i < dataArray.length(); i++) {
                 JSONObject tempData = dataArray.getJSONObject(i);
                 String tempId = tempData.get("id").toString();
-                chapList.add(tempId);
+                chapters.add(tempId);
             }
-            chapters.put(book, chapList);
             System.out.println(chapters);
         }
         return chapters;
+    }
+    public LinkedHashMap<String, ArrayList<Integer>> getSections(){
+        return null;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,55 @@
 package org.example;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        BibleAPI Bible = new BibleAPI();
-        ArrayList<String> books = new ArrayList<>();
-        LinkedHashMap<String, ArrayList<String>> chapters = new LinkedHashMap<>();
-        try {
-            String bibleId = Bible.getBible();
-            books = Bible.getBooks(bibleId);
-            chapters = Bible.getChapters(books, bibleId);
-        }
-        catch(Exception err){
-            System.out.println(err);
-        }
 
+        while(true) {
+
+            //Variables
+            BibleAPI Bible = new BibleAPI();
+            ArrayList<String> books = new ArrayList<>();
+            ArrayList<String> chapters = new ArrayList<>();
+            Scanner input = new Scanner(System.in);
+            String bibleId = "";
+
+            //Main Program Loop
+            System.out.println("Please enter the translation of the Bible you are interested in...");
+            System.out.println("If you wish to see a list type in 'bibles'");
+            String biblesInput = input.nextLine();
+
+            //Get BibleID Section
+            switch(biblesInput){
+                case "bibles":
+                    try {
+
+                    } catch(Exception err) {
+                        System.out.println(err);
+                    }
+                    break;
+                default: {
+                    try {
+                        bibleId = Bible.getBible(biblesInput);
+                    } catch(Exception err) {
+                        System.out.println(err);
+                    }
+                    break;
+                }
+            }
+            //Get Bible Books
+            //Get Bible Chapters
+            try {
+                books = Bible.getBooks(bibleId);
+                chapters = Bible.getChapters(books, bibleId);
+            }
+            catch(Exception err){
+                System.out.println(err);
+            }
+        }
     }
 }
